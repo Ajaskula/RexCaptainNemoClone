482399df8d191d5e94b5b4348ba8a42ed5cdf073
Bartosz Zbiciak <bz448593@students.mimuw.edu.pl>
2025-01-09 21:12:27 +0100

    Move systems to designated plugins

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,45 @@ use solid_walls::systems::*;
 use std::time::Duration;
 use walls::systems::*;
 
+struct LevelPlugin;
+struct GameplayPlugin;
+
+impl Plugin for LevelPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_solid_walls)
+            .add_systems(Startup, spawn_wall_rectangles)
+            .add_systems(Startup, spawn_player)
+            .add_systems(Startup, spawn_camera)
+            .add_systems(Startup, play_background_music)
+            .add_systems(Startup, set_background)
+            .add_systems(Startup, setup_collision)
+            .add_systems(Startup, spawn_rocks)
+            .add_systems(Startup, setup_bomb)
+            .add_systems(Startup, spawn_bombs)
+            .add_systems(Startup, spawn_enemy)
+            .add_systems(Startup, spawn_enemies)
+            .add_systems(Startup, spawn_exit)
+            .add_systems(Startup, spawn_falling_bomb);
+    }
+}
+
+impl Plugin for GameplayPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, player_movement)
+            .add_systems(Update, update_camera)
+            .add_systems(Update, player_dig_dirt)
+            .add_systems(Update, player_collect_bomb)
+            .add_systems(Update, plant_bomb_system)
+            .add_systems(Update, remove_explosion_sprite)
+            .add_systems(Update, explosive_lifetime_system)
+            .add_systems(Update, explodable_lifetime_system)
+            .add_systems(Update, enemy_movement)
+            .add_systems(Update, player_hit_enemy)
+            .add_systems(Update, moveable_elements_movement)
+            .add_systems(Update, player_push_system)
+            .add_systems(Update, enemy_hit_moveable_element);
+    }
+}
 fn main() {
     App::new()
         .init_resource::<BombCount>()
@@ -40,43 +79,8 @@ fn main() {
         })
         .insert_resource(PushCooldownTimer::default())
         .add_plugins(DefaultPlugins)
-        // .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        // .add_plugins(LogDiagnosticsPlugin::default())
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, play_background_music)
-        .add_systems(Startup, set_background)
-        .add_systems(Startup, spawn_player)
-        .add_systems(Startup, setup_collision)
-        .add_systems(Startup, spawn_solid_walls)
-        .add_systems(Startup, spawn_rocks)
-        .add_systems(Startup, spawn_falling_bomb)
-        .add_systems(Startup, spawn_plague_l)
-        .add_systems(Startup, spawn_plague_m)
-        .add_systems(Startup, spawn_plague_r)
-        // .add_systems(Startup, spawn_walls)
-        .add_systems(Startup, spawn_dirt)
-        .add_systems(Startup, spawn_rock_on_tower)
-        .add_systems(Startup, spawn_full_dirt_rectangles)
-        .add_systems(Startup, spawn_wall_rectangles)
-        .add_systems(Startup, setup_bomb)
-        .add_systems(Startup, spawn_bombs)
-        .add_systems(Startup, spawn_enemy)
-        .add_systems(Startup, spawn_enemies)
-        .add_systems(Startup, spawn_exit)
-        .add_systems(Update, player_movement)
-        .add_systems(Update, update_camera)
-        .add_systems(Update, player_dig_dirt)
-        .add_systems(Update, player_collect_bomb)
-        .add_systems(Update, plant_bomb_system)
-        .add_systems(Update, remove_explosion_sprite)
-        .add_systems(Update, explosive_lifetime_system)
-        .add_systems(Update, explodable_lifetime_system)
-        .add_systems(Update, enemy_movement)
-        .add_systems(Update, player_hit_enemy)
-        // .add_systems(Update, rock_movement)
-        // .add_systems(Update, falling_bomb_movement)
-        .add_systems(Update, moveable_elements_movement)
-        .add_systems(Update, player_push_system)
-        .add_systems(Update, enemy_hit_moveable_element)
+        .add_plugins((LevelPlugin, GameplayPlugin))
+        //.add_plugins(FrameTimeDiagnosticsPlugin::default())
+        //.add_plugins(LogDiagnosticsPlugin::default())
         .run();
 }
